1db0d0a60973b99dccdfb3abaed16bbe24b3e61d
AVincenzi <alessandro84.vincenzi@gmail.com>
2017-04-01 14:54:11 +0200

    Added bowling.Game.scoreForGame

--- test/test/TestGame.java
+++ test/test/TestGame.java
@@ -15,4 +15,23 @@ public class TestGame {
 		assertEquals(5, g.score());
 	}
 
+	@Test
+	public void twoThrowsNoMark() {
+		Game g = new Game();
+		g.add(5);
+		g.add(4);
+		assertEquals(9, g.score());
+	}
+
+	@Test
+	public void fourThrowsNoMark() {
+		Game g = new Game();
+		g.add(5);
+		g.add(4);
+		g.add(7);
+		g.add(2);
+		assertEquals(18, g.score());
+		assertEquals(9,  g.scoreForFrame(1));
+		assertEquals(18, g.scoreForFrame(2));
+	}
 }
